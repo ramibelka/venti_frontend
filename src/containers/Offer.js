b6b88c50722906d89c519f4ac8d0f87baba883a6
ramibelka <70215957+ramibelka@users.noreply.github.com>
2024-05-26 22:18:56 +0100

    Update Offer.js

--- src/containers/Offer.js
+++ src/containers/Offer.js
@@ -39,7 +39,7 @@ const Offer = ({ userToken }) => {
 
   const fetchData = async () => {
     try {
-      const response = await axiosInstance.get(`/api/articles/${id}`);
+      const response = await axiosInstance.get(`/api/articles/${id}/`);
       // const response = await axios.get(
       //   `http://127.0.0.1:8000/api/articles/${id}`
       // );
